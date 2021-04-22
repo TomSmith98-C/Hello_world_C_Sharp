e6fbde3aee464826a1ca4dab8fa3942ff212474b
Tom Smith <74121145+TomSmith98-C@users.noreply.github.com>
2021-04-22 09:12:44 -0500

    Add files via upload

---
+++ Primer_App/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Primer_App
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+                Console.WriteLine("Como estas");
+                Console.WriteLine("Hello World!");
+        }
+    }
+}
